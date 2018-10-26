f0dc2cc1a7f2cdd9ba21d04e0dc8bc62e9f6c59c
Meg <margaret.farley@tufts.edu>
2018-10-25 21:23:04 -0400

    better functionality

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -5,6 +5,37 @@
           center: {lat: 42.352271, lng: -71.05524200000001},
           zoom: 14
         };
+
+
+        function haversineDistance(lat,lng, Marker2) {
+                  function toRad(x) {
+                        return x * Math.PI / 180;
+                  }
+
+                var lon1 = lng;
+                //console.log("lon1 is " +lon1);
+                var lat1 = lat;
+                //console.log(lat1);
+                var lon2 = Marker2.lng;
+                //console.log(lon2);
+                var lat2 = Marker2.lat;
+                //console.log(lat2);
+
+                var R = 6371; // km
+
+                var x1 = lat2 - lat1;
+                var dLat = toRad(x1);
+                var x2 = lon2 - lon1;
+                var dLon = toRad(x2)
+                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var d = R * c;
+                d /= 1.60934;
+                return d;
+        }
+
         function gettraindata(Marker){
         //console.log("in function");
 
@@ -73,11 +104,12 @@
 
          var Alewife = new google.maps.Marker({
           position: {lat: 42.395428, lng: -71.142483},
-          title: 'Hello World!',
+          title: 'Alewife',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-alfcl',
         });
-        stationarr.push(Alewife.position);
+        var coords = {name:'Alewife',lat: 42.395428, lng: -71.142483};
+        stationarr.push(coords);
         //console.log(Alewife.position);
         //console.log(stationarr[0]);
         Alewife.setMap(map);
@@ -92,116 +124,127 @@
 
         var Davis = new google.maps.Marker({
           position: {lat: 42.39674 , lng: -71.121815},
-          title: 'Hello World!',
+          title: 'Davis-Square',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-davis'
         });
-        stationarr.push(Davis.position);
+        var coords = {name:'Davis' ,lat: 42.39674 , lng: -71.121815};
+        stationarr.push(coords);
         Davis.setMap(map);
         gettraindata(Davis);
 
 
         var Porter = new google.maps.Marker({
           position: {lat:  42.3884 , lng: -71.11914899999999},
-          title: 'Hello World!',
+          title: 'Porter-Square',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-portr'
         });
-        stationarr.push(Porter.position);
+        var coords = {name:'Porter',lat:  42.3884 , lng: -71.11914899999999};
+        stationarr.push(coords);
         Porter.setMap(map);
         gettraindata(Porter);
 
         var Harvard = new google.maps.Marker({
           position: {lat: 42.373362, lng: -71.118956},
-          title: 'Hello World!',
+          title: 'Harvard-Square',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-harsq'
         });
-        stationarr.push(Harvard.position);
+        var coords = {name:'Harvard',lat: 42.373362, lng: -71.118956};
+        stationarr.push(coords);
         Harvard.setMap(map);
         gettraindata(Harvard);
 
 
         var Central = new google.maps.Marker({
           position: {lat: 42.365486, lng:  -71.103802},
-          title: 'Hello World!',
+          title: 'Central-Square',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-cntsq'
         });
-        stationarr.push(Central.position);
+        var coords = {name:'Central',lat: 42.365486, lng:  -71.103802};
+        stationarr.push(coords);
         Central.setMap(map);
         gettraindata(Central);
 
 
         var Kendall = new google.maps.Marker({
           position: {lat: 42.36249079, lng:  -71.08617653},
-          title: 'Hello World!',
+          title: 'Kendall-Square',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-knncl'
         });
-        stationarr.push(Kendall.position);
+        var coords = {name:'Kendall',lat: 42.36249079, lng:  -71.08617653};
+        stationarr.push(coords);
         Kendall.setMap(map);
         gettraindata(Kendall);
 
         var Charles = new google.maps.Marker({
           position: {lat: 42.361166, lng:  -71.070628},
-          title: 'Hello World!',
+          title: 'Charles/MGH',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-chmnl'
         });
-        stationarr.push(Charles.position);
+        var coords = {name:'Charles',lat: 42.361166, lng:  -71.070628};
+        stationarr.push(coords);
         Charles.setMap(map);
         gettraindata(Charles);
   
 
         var Park = new google.maps.Marker({
           position: {lat: 42.35639457, lng:  -71.0624242},
-          title: 'Hello World!',
+          title: 'Park-Street',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-pktrm'
         });
-        stationarr.push(Park.position);
+        var coords = {name:'Park',lat: 42.35639457, lng:  -71.0624242};
+        stationarr.push(coords);
         Park.setMap(map);
         gettraindata(Park);
 
         var Crossing = new google.maps.Marker({
           position: {lat: 42.355518, lng:  -71.060225},
-          title: 'Hello World!',
+          title: 'Downtown-Crossing',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-dwnxg'
         });
-        stationarr.push(Crossing.position);
+        var coords = {name:'Downtown-Crossing',lat: 42.355518, lng:  -71.060225};
+        stationarr.push(coords);
         Crossing.setMap(map);
         gettraindata(Crossing);
 
 
         var South = new google.maps.Marker({
           position: {lat: 42.352271, lng:  -71.05524200000001},
-          title: 'Hello World!',
+          title: 'South-Station',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-sstat'
         });
-        stationarr.push(South.position);
+        var coords = {name:'South-Station',lat: 42.352271, lng:  -71.05524200000001};
+        stationarr.push(coords);
         South.setMap(map);
         gettraindata(South);
 
         var Broadway = new google.maps.Marker({
           position: {lat: 42.342622, lng:  -71.056967},
-          title: 'Hello World!',
+          title: 'Broadway',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-brdwy'
         });
-        stationarr.push(Broadway.position);
+        var coords = {name:'Broadway',lat: 42.342622, lng:  -71.056967};
+        stationarr.push(coords);
         Broadway.setMap(map);
         gettraindata(Broadway);
 
         var Andrew = new google.maps.Marker({
           position: {lat: 42.330154, lng: -71.057655},
-          title: 'Hello World!',
+          title: 'Andrew',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-andrw'
         });
-        stationarr.push(Andrew.position);
+        var coords = {name:'Andrew',lat: 42.330154, lng: -71.057655};
+        stationarr.push(coords);
         Andrew.setMap(map);
         gettraindata(Andrew);
 
@@ -209,114 +252,130 @@
 
         var JFK = new google.maps.Marker({
           position: {lat: 42.320685, lng: -71.052391},
-          title: 'Hello World!',
+          title: 'JFK',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-jfk'
         });
-        stationarr.push(JFK.position);
-        redbreak.push(JFK.position);
+        var coords = {name:'JFK',lat: 42.320685, lng: -71.052391};
+        stationarr.push(coords);
+        redbreak.push(JFK);
         JFK.setMap(map);
         gettraindata(JFK);
 
         var NorQuincy = new google.maps.Marker({
           position: {lat: 42.275275, lng: -71.029583},
-          title: 'Hello World!',
+          title: 'North-Quincy',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-nqncy'
         });
 
-        redbreak.push(NorQuincy.position);
+        redbreak.push(NorQuincy);
+        var coords = {name:'North-Quincy',lat: 42.275275, lng: -71.029583};
+        stationarr.push(coords);
         NorQuincy.setMap(map);
         gettraindata(NorQuincy);
 
 
         var Wollaston = new google.maps.Marker({
           position: {lat: 42.2665139, lng: -71.0203369},
-          title: 'Hello World!',
+          title: 'Wollaston',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-wlsta'
         });
 
-        redbreak.push(Wollaston.position);
+        redbreak.push(Wollaston);
+        var coords = {name:'Wollaston',lat: 42.2665139, lng: -71.0203369};
+        stationarr.push(coords);
         Wollaston.setMap(map);
         gettraindata(Wollaston);
        
 
         var Qcenter = new google.maps.Marker({
           position: {lat: 42.251809, lng: -71.005409},
-          title: 'Hello World!',
+          title: 'Quincy-center',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-qnctr'
         });
 
-        redbreak.push(Qcenter.position);
+        redbreak.push(Qcenter);
+        var coords = {name:'Quincy-center',lat: 42.251809, lng: -71.005409};
+        stationarr.push(coords);
         Qcenter.setMap(map);
         gettraindata(Qcenter);
 
         var Qadams = new google.maps.Marker({
           position: {lat: 42.233391, lng: -71.007153},
-          title: 'Hello World!',
+          title: 'Quincy-adams',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-qamnl'
         });
 
-        redbreak.push(Qadams.position);
+        redbreak.push(Qadams);
+        var coords = {name:'Quincy-adams',lat: 42.233391, lng: -71.007153};
+        stationarr.push(coords);
         Qadams.setMap(map);
         gettraindata(Qadams);
 
     
         var Braintree = new google.maps.Marker({
           position: {lat: 42.2078543, lng: -71.0011385},
-          title: 'Hello World!',
+          title: 'Braintree',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-brntn'
         });
 
-        redbreak.push(Braintree.position);
+        redbreak.push(Braintree);
+        var coords = {name:'Braintree',lat: 42.2078543, lng: -71.0011385};
+        stationarr.push(coords);
         Braintree.setMap(map);
         gettraindata(Braintree);
 
 
         var Savhill = new google.maps.Marker({
           position: {lat: 42.31129, lng: -71.053331},
-          title: 'Hello World!',
+          title: 'Savhill',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-shmnl'
         });
-        stationarr.push(Savhill.position);
+        var coords = {name:'Savhill',lat: 42.31129, lng: -71.053331};
+        stationarr.push(coords);
         Savhill.setMap(map);
         gettraindata(Savhill);
 
         var Fieldscorn = new google.maps.Marker({
           position: {lat: 42.300093, lng: -71.061667},
-          title: 'Hello World!',
+          title: 'Fieldscorn',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-fldcr'
         });
-        stationarr.push(Fieldscorn.position);
+        var coords = {name:'Fieldscorn',lat: 42.300093, lng: -71.061667};
+        stationarr.push(coords);
         Fieldscorn.setMap(map);
         gettraindata(Fieldscorn);
 
         var Shawmut = new google.maps.Marker({
           position: {lat: 42.29312583, lng: -71.06573796000001},
-          title: 'Hello World!',
+          title: 'Shawmut',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-smmnl'
         });
-        stationarr.push(Shawmut.position);
+        var coords = {name:'Shawmut',lat: 42.29312583, lng: -71.06573796000001};
+        stationarr.push(coords);
         Shawmut.setMap(map);
         gettraindata(Shawmut);
         
 
         var Ashmont = new google.maps.Marker({
           position: {lat: 42.284652, lng: -71.06448899999999},
-          title: 'Hello World!',
+          title: 'Ashmont',
           icon: 'icons8-arrow-pointing-down-26.png',
           stop_id: 'place-asmnl'
         });
-        stationarr.push(Ashmont.position);
+        var coords = {name:'Ashmont',lat: 42.284652, lng: -71.06448899999999};
+        stationarr.push(coords);
         Ashmont.setMap(map);
         gettraindata(Ashmont);
+
 
         var redpath = [
         {lat: 42.395428, lng: -71.142483},
@@ -389,15 +448,61 @@
             //var distance = google.maps.geometry.spherical.computeDistanceBetween(pos,{lat: 42.39674 , lng: -71.121815}); 
             //var contentsring = ''    
 
-            var infowindow = new google.maps.InfoWindow({
-          		content: 'Closest T stop is'
-        	});
-
             var marker = new google.maps.Marker({
           		position: pos,
           		map: map,
-         	 	title: 'Hello World!'
-        	});
+         	 	  title: 'Hello World!'
+        	 });
+            stationdistance = [];
+            //console.log(stationarr.length);
+            small = 0;
+            for(var i = 0; i < stationarr.length; i++){
+            //   //console.log(stationarr[i].title);
+            //   var mark1 = stationarr[i];
+            //   var pos1 = mark1.position;
+            //   //console.log(pos1);
+            //   var lat = pos1.lat;
+            //   //console.log(lat);
+            //   var lng = pos1.lng;
+            //   //console.log(lng);
+            //   var title = mark1.title;
+            //   console.log(Alewife.position);
+              var stationmiles = {name: stationarr[i].name, miles: haversineDistance(stationarr[i].lat, stationarr[i].lng, pos)};
+
+            //   console.log("miles: " + stationmiles.miles);
+                stationdistance.push(stationmiles);
+                if(stationdistance[i].miles < stationdistance[small].miles){
+                    small = i;
+              }
+            }
+
+            //var mark1 = stationarr[i];
+            //console.log(mark1);
+          var infowindow = new google.maps.InfoWindow({
+              content: 'Closest T stop is ' + stationdistance[small].name
+          });
+          var pos1 = {
+              lat: stationarr[small].lat,
+              lng: stationarr[small].lng
+            };
+
+          var closest  = [
+          pos1,
+          pos
+          ];
+
+          var TPath = new google.maps.Polyline({
+              path: closest,
+              geodesic: true,
+              strokeColor: '#FFF000',
+              strokeOpacity: 1.0,
+              strokeWeight: 2
+          });
+
+          TPath.setMap(map);
+
+
+
         	//marker.setMap(map);
         	marker.addListener('click', function() {
           		infowindow.open(map, marker);
